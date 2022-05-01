bfbb374a04bd54fd042fe3c42110464ed3c9a0af
360mtucker <46805953+360mtucker@users.noreply.github.com>
2022-05-01 14:09:56 -0700

    added problem-set-4

---
+++ problem-set-4.py
@@ -0,0 +1,57 @@
+"""
+*********I just want you to know that you are doing a good job.  Keep up the good work, I *********know it takes a lot to work on a program this difficult.  If you need help, reach *********out to me.  I am doing sessions with students on the weekends and evenings to meet *********their availability.  V/R - Professor Tucker.  mtucker@clark.edu.  360.947.6011
+
+dHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHb
+HHP%%#%%%%%%%%%%%%%%%%#%%%%%%%#%%VHH
+HH%%%%%%%%%%#%v~~~~~~\%%%#%%%%%%%%HH
+HH%%%%%#%%%%v'        ~~~~\%%%%%#%HH
+HH%%#%%%%%%v'dHHb      a%%%#%%%%%%HH
+HH%%%%%#%%v'dHHHA     :%%%%%%#%%%%HH
+HH%%%#%%%v' VHHHHaadHHb:%#%%%%%%%%HH
+HH%%%%%#v'   `VHHHHHHHHb:%%%%%#%%%HH
+HH%#%%%v'      `VHHHHHHH:%%%#%%#%%HH
+HH%%%%%'        dHHHHHHH:%%#%%%%%%HH
+HH%%#%%        dHHHHHHHH:%%%%%%#%%HH
+HH%%%%%       dHHHHHHHHH:%%#%%%%%%HH
+HH#%%%%       VHHHHHHHHH:%%%%%#%%%HH
+HH%%%%#   b    HHHHHHHHV:%%%#%%%%#HH
+HH%%%%%   Hb   HHHHHHHV'%%%%%%%%%%HH
+HH%%#%%   HH  dHHHHHHV'%%%#%%%%%%%HH
+HH%#%%%   VHbdHHHHHHV'#%%%%%%%%#%%HH
+HHb%%#%    VHHHHHHHV'%%%%%#%%#%%%%HH
+HHHHHHHb    VHHHHHHH:%odHHHHHHbo%dHH
+HHHHHHHHboodboooooodHHHHHHHHHHHHHHHH
+HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+VHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHV
+
+                              _           
+                             (_)          
+ _ __   ___ _ __   __ _ _   _ _ _ __  ___ 
+| '_ \ / _ \ '_ \ / _` | | | | | '_ \/ __|
+| |_) |  __/ | | | (_| | |_| | | | | \__ \
+| .__/ \___|_| |_|\__, |\__,_|_|_| |_|___/
+| |                __/ |                  
+|_|               |___/             
+
+
+NTEC 365 - Spring 2022
+YOUR NAME
+Date: 
+Name: Module 4 - Problem Set 4
+Description: Problem X
+"""
+
+""" 
+IPO
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Input(s): 
+Process(es): 
+Output(s): 
+"""
+
+
+def main():
+    pass  # remove this line after you write your code
+
+
+main()
